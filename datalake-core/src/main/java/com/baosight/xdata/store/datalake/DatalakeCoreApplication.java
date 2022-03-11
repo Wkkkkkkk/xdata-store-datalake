a88b9ea6102b3003cd35ead59448ae4afa090463
wuke <bigbigblame@163.com>
2022-03-11 09:53:58 +0800

    test

---
+++ datalake-core/src/main/java/com/baosight/xdata/store/datalake/DatalakeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.baosight.xdata.store.datalake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalakeCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatalakeCoreApplication.class, args);
+    }
+
+}
